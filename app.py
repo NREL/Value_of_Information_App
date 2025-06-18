1370a9e6e24b96a9d05057629da16fd1f3a97baa
Menon <kmenon@nrel.gov>
2025-06-18 11:18:40 -0600

    update app

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ from mpl_toolkits.axes_grid1.inset_locator import inset_axes
 from geophires_x_client import GeophiresXClient
 from geophires_x_client.geophires_input_parameters import GeophiresInputParameters
 
-
+from GEOPHIRES_X import Geophires_output
 from User_input import st_file_selector, Prior_probability_binary, make_value_array
 from Naive_Bayes import make_train_test, optimal_bin
 from Bayesian_Modeling import likelihood_KDE, Scaledlikelihood_KDE, Posterior_by_hand, Posterior_Marginal_plot, Posterior_via_NaiveBayes
@@ -195,7 +195,7 @@ with st.sidebar:
                  label=':blue-background[**Click here:\n for feedback**]',icon=":material/cloud:")
 
     st.page_link("https://github.com/NREL/Value_of_Information_App/tree/main/File%20Template",\
-                 label=':orange-background[**Click here:\n file templates & examples**]',icon=":material/question_exchange:")
+                 label=':orange-background[**Click here:\n User Manual, file templates & examples**]',icon=":material/question_exchange:")
     uploaded_files = st.file_uploader(\
         "Upload two data files,namely a Positive Label file (\'POS_\' :fire:) & a Negative Label (\'NEG_\':thumbsdown:) file ", \
                                       type=['csv'],accept_multiple_files=True)
@@ -342,149 +342,15 @@ if uploaded_files is not None:
             type_geo = 2
         else:
             type_geo = 1
-        client = GeophiresXClient()
-        result = client.get_geophires_result(
-                    GeophiresInputParameters({
-                        "Gradient 1": gradient,
-                        "Reservoir Depth": depth,
-                        "End-Use Option": type_geo,
-                        "Power Plant Type": "4",
-                        "Economic Model": "3",
-                        "Starting Electricity Sale Price": "0.15",
-                        "Ending Electricity Sale Price": "1.00",
-
-                        #"Reservoir Heat Capacity": "790",
-                        #"Reservoir Thermal Conductivity": "3.05",
-                        #"Reservoir Porosity": "0.0118",
-                        #"Reservoir Impedance": "0.01",
-                        #"Number of Fractures": "108",
-                        #"Fracture Shape": "4",
-                        #"Fracture Height": "300",
-                        #"Fracture Width": "400",
-                        #"Fracture Separation": "30",
-                        #"Number of Production Wells": "1",
-                        #"Number of Injection Wells": "1", Keep out for now, add in later
-                    })
-                )
-
-    
-
-        with open(result.output_file_path, 'r') as f:
-        #print(f.read())
-        #print(f.read(1500))
-            words = ['Project NPV:','Drilling and completion costs per well:']
-                       
-            lines = f.readlines()
-                              
-            num = 30            
-            # Manually setting since parsing is not working
-
-            #for row1 in range(len(lines)):
-                ## check if string present on a current line
-                #row = lines[row1]
-                #st.write(row)
-                #word = 'Project NPV:'
-                
-                #print(row.find(word))
-                # find() method returns -1 if the value is not found,
-                # if found it returns index of the first occurrence of the substring
-                #if row.find(word) != -1:
-                    #st.write('string exists in file')
-                    #st.write('line Number:', lines.index(row))
-                    #no = lines.index(row)
-                    #no = row1
-
-            
-            npv = str(lines[num-1:num]) # Drilling and completion costs
-            npv1= npv.split(':')
-            npv1 = npv.replace(" ","")
-            npv1 = npv1.replace('\n',"")
-            npv1 = npv1.split('MUSD')
-            
-            npv2 = npv1[0:1]
-            
-            npv2 = str(npv2)
-            npvv = npv2.split(':')
-            final_npv = npvv[1:2]
-            aa = str(final_npv[0:1])
-            val = (''.join(c for c in aa if (c.isdigit() or c =='.' or c =='-')))
-            val2 = (val.strip())
-     
-            val2 = float(val2)
-
-            
-            npv_final = val2*1e6
-            
-
-            num = 31            
-            # Manually setting since word is not working
-            #for row1 in range(len(lines)):
-                ## check if string present on a current line
-                #row = lines[row1]
-                #st.write(row)
-                #word = 'Project NPV:'
-                
-                #print(row.find(word))
-                # find() method returns -1 if the value is not found,
-                # if found it returns index of the first occurrence of the substring
-                #if row.find(word) != -1:
-                    #st.write('string exists in file')
-                    #st.write('line Number:', lines.index(row))
-                    #no = lines.index(row)
-                    #no = row1
-
-            # For drilling cost
-           
-            num = 93 # Change to 95 in new one
-
-
-            npv = str(lines[num-1:num]) # Drilling and completion costs
         
-            npv1= npv.split(':')
-            npv1 = npv.replace(" ","")
-            npv1 = npv1.replace('\n',"")
-            npv1 = npv1.split('MUSD')
-            
-            npv2 = npv1[0:1]
-            
-            npv2 = str(npv2)
-            npvv = npv2.split(':')
-            final_npv = npvv[1:2]
-            aa = str(final_npv[0:1])
-            val = (''.join(c for c in aa if (c.isdigit() or c =='.' or c =='-')))
-            val2 = (val.strip())
-    
-            val2 = float(val2)
-            drill_cost = -1*val2*1e6
+        # Pulling in NPV and drilling costs from GEOPHIRES-X
+        no_prod = 2
+        no_inj = 2 # Default production and injection wells from GEOPHIRES- user can change if needed.
+        npv_final,drill_cost = Geophires_output(gradient,depth,type_geo,no_prod,no_inj)
 
-            num = 94
-            npv = str(lines[num-1:num]) # Drilling and completion costs
-        
-            npv1= npv.split(':')
-            npv1 = npv.replace(" ","")
-            npv1 = npv1.replace('\n',"")
-            npv1 = npv1.split('MUSD')
-            
-            npv2 = npv1[0:1]
-            
-            npv2 = str(npv2)
-            npvv = npv2.split(':')
-            final_npv = npvv[1:2]
-            aa = str(final_npv[0:1])
-            val = (''.join(c for c in aa if (c.isdigit() or c =='.' or c =='-')))
-            val2 = (val.strip())
-    
-            val2 = float(val2)
-            drill_cost2 = -1*val2*1e6
-            
-           
-        
-       
-        if (type_geo == 2):
-            drill_cost = drill_cost2
-        else:
-            drill_cost = drill_cost
 
+
+        
         # Table for Outcomes part with GEOPHIRES costs
         newValuedf = pd.DataFrame({
                "action": ['walk away','drill'],
